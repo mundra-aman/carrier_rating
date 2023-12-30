0ae98a2be815a98efb9c07204c097cde7c0f08f1
Aman <aman.adsm@gmail.com>
2023-12-30 13:50:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,11 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-sns.set()
+#import matplotlib.pyplot as plt
+#import seaborn as sns
+#sns.set()
 
 import streamlit as st
-import plotly
-import plotly.express as px
+#import plotly
+#import plotly.express as px
 
 
 st.set_page_config(page_title='Carrier-Master', 
